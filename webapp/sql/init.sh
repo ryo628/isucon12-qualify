841e7a25fd2eeb43e76ccae2895a1a759919eef5
plainbanana <plainbanana@mustardon.tokyo>
2023-10-13 01:20:20 +0900

    refactor: rename index

--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -20,4 +20,4 @@ mysql -u"$ISUCON_DB_USER" \
 rm -f ../tenant_db/*.db
 cp -r ../../initial_data/*.db ../tenant_db/
 
-find ../tenant_db/ -name "*.db" | xargs -IXXX sqlite3 XXX "CREATE INDEX tenant_id_competition_id_idx ON player_score ( competition_id, player_id );"
+find ../tenant_db/ -name "*.db" | xargs -IXXX sqlite3 XXX "CREATE INDEX competition_id_player_id_idx ON player_score ( competition_id, player_id );"
