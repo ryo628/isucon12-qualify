eae1a0e1c160247d39027a75fb95c643b33a147b
plainbanana <plainbanana@mustardon.tokyo>
2023-10-13 01:41:53 +0900

    feat: visit_history_2に複合インデックスtenant_id_competition_id_idxを追加
    スコア 78714 (+78714 0(0%))

--- webapp/sql/admin/10_schema.sql
+++ webapp/sql/admin/10_schema.sql
@@ -36,5 +36,6 @@ CREATE TABLE `visit_history_2`
     `tenant_id`      BIGINT UNSIGNED NOT NULL,
     `competition_id` VARCHAR(255) NOT NULL,
     `created_at`     BIGINT       NOT NULL,
-    INDEX            `tenant_id_idx` (`tenant_id`)
+    INDEX            `tenant_id_idx` (`tenant_id`),
+    INDEX            `tenant_id_competition_id_idx` (`tenant_id`, `competition_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
